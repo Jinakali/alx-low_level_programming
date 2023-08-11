35e2696f68555748dc7fdeac3d166a20c64ea4a9
Jinakali <mutuaericmurithi@gmail.com>
2023-08-11 05:05:32 -0700

    mixed case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main-entry point
+ *
+ *Return:0 when successful
+ */
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
