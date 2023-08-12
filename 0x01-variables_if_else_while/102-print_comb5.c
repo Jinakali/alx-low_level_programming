c1fa20631529dfe4d7a137cd4c5b9ef724a4403e
Jinakali <mutuaericmurithi@gmail.com>
2023-08-12 01:20:58 -0700

    final

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,47 +4,35 @@
  *
  *Return:0 when successful
  */
-#include <stdio.h>
 int main(void)
 {
-	int first_digit = 0;
+	int i, j;
 
+	i = 0;
 
-	while (first_digit <= 9)
+	while (i <= 98)
 	{
-		int second_digit = 0;
+		j = i + 1;
 
-		while (second_digit < 9)
-
+		while (j <= 99)
 		{
-			int third_digit = first_digit;
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 
-			while (third_digit <= 9)
+			if (!(i == 98 && j == 99))
 			{
-				int fourth_digit = 1;
-
-				while (fourth_digit <= 9)
-				{
-					putchar(first_digit + '0');
-					putchar(second_digit + '0');
-					putchar(' ');
-					putchar(third_digit + '0');
-					putchar(fourth_digit + '0');
-					if (first_digit != 9 || second_digit != 8 || third_digit != 9 ||
-fourth_digit != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					fourth_digit++;
-				}
-				third_digit++;
+				putchar(',');
+				putchar(' ');
 			}
-
-			second_digit++;
+			j++;
 		}
-		first_digit++;
+		i++;
 	}
+
 	putchar('\n');
+
 	return (0);
 }
