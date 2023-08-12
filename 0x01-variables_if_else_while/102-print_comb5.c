d21e5644c0acb4153be5d150be1d87f169c77013
Jinakali <mutuaericmurithi@gmail.com>
2023-08-11 20:26:39 -0700

    more unique numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,41 +5,42 @@
  *Return:0 when successful
  */
 #include <stdio.h>
-
 int main(void)
 {
 	int first_digit = 0;
+
 
 	while (first_digit <= 9)
 	{
 		int second_digit = 0;
 
-		while (second_digit <= 9)
+		while (second_digit <= 8)
+
 		{
-			int third_digit = 0;
+			int third_digit = first_digit;
 
 			while (third_digit <= 9)
 			{
-				int fourth_digit = 1;
+				int fourth_digit = second_digit + 1;
 
 				while (fourth_digit <= 9)
 				{
-					putchar('0');
 					putchar(first_digit + '0');
 					putchar(second_digit + '0');
+					putchar(' ');
 					putchar(third_digit + '0');
 					putchar(fourth_digit + '0');
-					if (first_digit != 9 || second_digit != 7 || third_digit != 9 || fourth_di
-git != 9)
+					if (first_digit != 9 || second_digit != 9 || third_digit != 9 ||
+fourth_digit != 9)
 					{
 						putchar(',');
 						putchar(' ');
 					}
-
 					fourth_digit++;
 				}
 				third_digit++;
 			}
+
 			second_digit++;
 		}
 		first_digit++;
