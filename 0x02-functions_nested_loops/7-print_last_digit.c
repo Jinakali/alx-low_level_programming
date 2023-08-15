296d2544f19d94d3c7a3f37ecf678f374860fcf2
Jinakali <mutuaericmurithi@gmail.com>
2023-08-15 13:59:55 -0700

    add

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_last_digit - print last digit
+ *@num: parameter is an integer
+ *
+ *Return:0
+ */
+int print_last_digit(int num)
+{
+	int i = num % 10;
+
+	if (i < 0)
+		i  = -i;
+	return (i);
+}
