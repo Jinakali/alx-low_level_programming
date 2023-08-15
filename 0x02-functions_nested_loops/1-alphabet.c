2daadcad27a53917206822de257837a58ba92998
Jinakali <mutuaericmurithi@gmail.com>
2023-08-15 10:27:56 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_alphabet - print the alphabet
+ */
+void print_alphabet(void)
+{
+	char b = 'a';
+
+	while (b <= 'z')
+	{
+		_putchar(b);
+		b++;
+	}
+	_putchar('\n');
+}
