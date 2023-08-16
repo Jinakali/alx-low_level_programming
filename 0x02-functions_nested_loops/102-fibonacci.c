45b5bb0087ebfa23667de1c5c5a11272b8988ad8
Jinakali <mutuaericmurithi@gmail.com>
2023-08-16 10:13:14 -0700

    series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void print_f(int limit);
+
+/**
+ *main - entry
+ *
+ *Return: 0 always
+ */
+int main(void)
+{
+	print_f(50);
+	return (0);
+}
+
+/**
+ * print_f - print fib series
+ *@limit: int  to  set range limit
+ */
+void print_f(int limit)
+{
+	 int i, result, a, b;
+
+	a = 1;
+	b = 2;
+	for (i = 0; i < limit; i++)
+	{
+		printf("%d", a);
+		if (i != (limit - 1))
+			printf(", ");
+		result = a + b;
+		a = b;
+		b = result;
+	}
+	printf("\n");
+}
