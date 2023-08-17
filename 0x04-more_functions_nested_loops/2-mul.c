03f462953443cea03a8c19b3fa097014afd836fd
Jinakali <mutuaericmurithi@gmail.com>
2023-08-17 08:48:47 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *mul -  check for uppercase
+  *@a:character to be checked
+  *@b:character to be multiplied
+  *
+  *Return: 0
+  */
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+
+	return (r);
+}
