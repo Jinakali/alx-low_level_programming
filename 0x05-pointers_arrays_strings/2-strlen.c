c8f0f4a5f6ffc1777b6653fe25764ba0f0e3bb9b
Jinakali <mutuaericmurithi@gmail.com>
2023-08-21 09:52:30 -0700

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen - print length of string
+ *@s: string whose length must be found
+ *
+ *Return: string length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+	
+	return (i);
+}
