7c4b449488fd0843a52d810bca1b4c86848c9407
Jinakali <mutuaericmurithi@gmail.com>
2023-08-24 12:28:04 -0700

    cap words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *cap_string - cap all words
+ *@str:string
+ *
+ *Return:pointer to string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == '\n' || str[i] == ' ' || str[i] == '\t' || str[i] == ',' ||
+str[i] == '.' || str[i] == ';' || str[i] == '!' || str[i] == '(' ||
+str[i] == ')' || str[i] == '{' || str[i] == '}' || str[i] == 34)
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] = str[i + 1] - 32;
+		}
+	}
+	return (str);
+}
