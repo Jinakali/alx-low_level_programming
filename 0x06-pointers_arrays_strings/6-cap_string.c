c6622e6364b53ba66eb842b2051e47edb44f30ee
Jinakali <mutuaericmurithi@gmail.com>
2023-08-24 12:38:03 -0700

    cap words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,8 @@ char *cap_string(char *str)
 	{
 		if (str[i] == '\n' || str[i] == ' ' || str[i] == '\t' || str[i] == ',' ||
 str[i] == '.' || str[i] == ';' || str[i] == '!' || str[i] == '(' ||
-str[i] == ')' || str[i] == '{' || str[i] == '}' || str[i] == 34)
+str[i] == ')' || str[i] == '{' || str[i] == '}' || str[i] == 34 ||
+str[i] == '?')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] = str[i + 1] - 32;
