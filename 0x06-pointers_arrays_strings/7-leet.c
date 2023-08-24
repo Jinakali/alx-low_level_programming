bb1a0f7f31ed394ea78a7d4448dc4710c9b8f600
Jinakali <mutuaericmurithi@gmail.com>
2023-08-24 14:37:15 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,14 +8,15 @@
 char *leet(char *str)
 {
 	int i, j;
-	char letter[] = {'a','e','o','t','l'};
-	char number[] = {'4','3','0','7','1'};
+	char letter[] = {'a', 'e', 'o', 't', 'l'};
+	char number[] = {'4', '3', '0', '7', '1'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (str[i] == letter[j] || (str[i] == letter[j] - 32 && str[i] >= 'a' && str[i] <= 'z') )
+			if (str[i] == letter[j] || (str[i] == letter[j] - 32 && str[i] >= 'a' &&
+str[i] <= 'z'))
 				str[i] = number[j];
 		}
 	}
