82e083693a044f6b70e9cd137e8dac798e2d5ddf
Jinakali <mutuaericmurithi@gmail.com>
2023-08-24 11:39:18 -0700

    uppercase conversion

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *-string_toupper - convert lower to uppercase
+ *@str:string
+ *
+ *Return:pointer to string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i =0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	str[i] = '\0';
+	return (str);
+}
