e4724495d9c76746147dd0e6a821c3426326e5ae
Jinakali <mutuaericmurithi@gmail.com>
2023-08-28 13:24:52 -0700

    board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - print an 8 by 8 board
+ * @a: array of arrays
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
