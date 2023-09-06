f9c29cf1542949c5adbb3ea3dd54480ae187ee64
Jinakali <mutuaericmurithi@gmail.com>
2023-09-06 11:16:22 -0700

    2d grid correction

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,14 +14,20 @@ int **alloc_grid(int width, int height)
 
 	if (width == 0 || height == 0)
 		return (NULL);
-	grid_arr = malloc(sizeof(int *) * (height));
+	grid_arr = malloc(sizeof(int) * (height));
 	if (grid_arr == NULL)
+	{
 		return (NULL);
+		free(grid_arr);
+	}
 	for (i = 0; i < width; i++)
 	{
 		grid_arr[i] = malloc(width * sizeof(int));
 		if (grid_arr[i] == NULL)
+		{
 			return (NULL);
+			free(grid_arr);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
