761801ff16c05d4b377eae06802c88b1ded4f1ec
Jinakali <mutuaericmurithi@gmail.com>
2023-09-11 14:52:11 -0700

    link

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,8 +15,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = name;
+
+	d->name =_strdup(name);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
 	d->age = age;
-	d->owner = owner;
+	d->owner = _strdup(owner);
+	if (d-> owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+
 	return (d);
 }
