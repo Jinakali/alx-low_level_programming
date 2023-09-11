b6e7e8707916d65632ce00703ed8e565be4dfb06
Jinakali <mutuaericmurithi@gmail.com>
2023-09-11 12:40:02 -0700

    readme & struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - contain dog details
+ *@name:pointer to string literal
+ *@age:age of dog
+ *@owner:name of custofian
+ *
+ *Description: dog details
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
