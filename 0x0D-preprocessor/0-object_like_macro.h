2419ec413a013fad29ba2718f09b60b32e8e587b
Jinakali <mutuaericmurithi@gmail.com>
2023-09-20 01:14:00 -0700

    defer

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef OBJECT_LIKE_MACRO_H_
-#define OBJECT_LIKE_MACRO_H_
-
-#define SIZE 1024
-
-#endif /*0-OBJECT_LIKE_MACRO_H*/
--- 100-atoi.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-/**
-  *_atoi - convert a string to an integer
-  *@s: string
-  *
-  *Return: the number
-  */
-int _atoi(char *s);
