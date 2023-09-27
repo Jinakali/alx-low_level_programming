1239cf41a7d1a037b4333feaf8826242bf7a0ef6
Jinakali <mutuaericmurithi@gmail.com>
2023-09-27 11:51:51 -0700

    palindrome recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -19,3 +19,16 @@ int is_palindrome(char *s)
 	}
 	return (1);
 }
+/**
+ *_strlen_recursion-_strlen via recursion
+ *@s:string
+ *Return: strlength
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (_strlen_recursion(s + 1) + 1);
+}
