c28d4030cda46a06d169f529916fb00693dc988b
Jinakali <mutuaericmurithi@gmail.com>
2023-10-02 09:49:26 -0700

    fix:null

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -9,15 +9,18 @@
  */
 int main(void)
 {
-    char *s;
+    char *s, *i, *j, *k;
 
     s = str_concat("Betty ", "Holberton");
+    i = str_concat("Hello", NULL);
+    j = str_concat(NULL, "Hello");
+    k = str_concat(NULL, NULL);
     if (s == NULL)
     {
         printf("failed\n");
         return (1);
     }
-    printf("%s\n", s);
+    printf("%s\n%s\n%s\n%s\n", s, i, j, k);
     free(s);
     return (0);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,7 +27,7 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k;
 	char *dest;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
