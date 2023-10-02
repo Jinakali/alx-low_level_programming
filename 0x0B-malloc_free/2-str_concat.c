306b8b790ffa87de7c7ed344645f2efc3059b03d
Jinakali <mutuaericmurithi@gmail.com>
2023-10-02 09:22:18 -0700

    strncat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _strlen - string length
+ *@s:pointer to the string
+ *
+ *Return: string length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+/**
+ *str_concat -concatenates two strings.
+ *@s1: destination
+ *@s2: string to be added to s1
+ *Return: duplicated string or NULL;
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k;
+	char *dest;
+
+	j = _strlen(s1);
+
+	dest = malloc(sizeof(char) * (j + _strlen(s2)) + 1);
+
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		dest[i] = s1[i];
+	for (k = 0; s2[k] != '\0'; k++)
+		dest[i + k] = s2[k];
+	dest[i + k] = '\0';
+
+	return (dest);
+}
