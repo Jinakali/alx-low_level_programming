ff926aea238085431fab348cbea88966eb976fb6
Jinakali <mutuaericmurithi@gmail.com>
2023-10-05 04:10:06 -0700

    array range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *array_range-allocates memory for an array, using malloc.Mem initialised
+  *@min:ist array element
+  *@max: last element in array
+  *Return: ptr at beginning of mem or NULL
+  */
+int *array_range(int min, int max)
+{
+	int *ptr, i, array_size;
+
+	if (min > max)
+		return (NULL);
+	array_size = (max - min) + 1;
+	ptr = malloc(array_size * sizeof(int));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i <= array_size; i++)
+		ptr[i] = min++;
+	return (ptr);
+}
