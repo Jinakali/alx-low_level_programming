45169c5572fde1f6d1c367578382ddffaaadb3b7
Jinakali <mutuaericmurithi@gmail.com>
2023-10-09 03:03:57 -0700

    2 tasks

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+  *init_dog-initialise a variable of type struct dog
+  *@d:pointer to variable passed
+  *@name:dog's name
+  *@age: dog's age
+  *@owner:owner"s name
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+  *struct dog - store dog's details
+  *@name:dog's name
+  *@age:dog's age
+  *@owner:dog's owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif /*dog.h*/
