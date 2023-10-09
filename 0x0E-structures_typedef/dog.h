84a4b0cafa2d7796111d8a759225a8a5b118fbc3
Jinakali <mutuaericmurithi@gmail.com>
2023-10-09 06:44:01 -0700

    fix:typedef declaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,11 @@
   *@age:dog's age
   *@owner:dog's owner
   */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typdef struct dog dog_t;
 #endif /*dog.h*/
