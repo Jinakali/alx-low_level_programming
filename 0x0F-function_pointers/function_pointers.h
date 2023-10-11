7fba8a697a014bc0fa623e5bca873ebd15b7d7ed
Jinakali <mutuaericmurithi@gmail.com>
2023-10-11 07:37:23 -0700

    fix handle null in all first 3 tasks

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS_H_
-#define FUNCTION_POINTERS_H_
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 #include <stddef.h>
 #include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
