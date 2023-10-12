1ecdb9c3d0d261eb1a66ee98df408f07c7f74111
Jinakali <mutuaericmurithi@gmail.com>
2023-10-12 06:04:04 -0700

    fix: betty task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,8 +1,8 @@
 #include "variadic_functions.h"
 /**
-  *print_numbers-prints numbers, followed by a new line
+  *print_strings-prints strings, followed by a new line
   *@separator:string to be printed between numbers
-  *@n:number of integers passed to the function
+  *@n:number of strings passed to the function
   */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
