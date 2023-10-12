50593eeadb8ccfcc8fe2a06a19ea23091e95ace2
Jinakali <138693861+Jinakali@users.noreply.github.com>
2023-10-12 22:31:24 +0300

    consider NULL format string

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -56,7 +56,7 @@ void print_all(const char * const format, ...)
 	va_start(arg, format);
 
 	i = 0;
-	while (format[i] != '\0')
+	while (format[i] != '\0' && format != NULL)
 	{
 		j = 0;
 		while (arr[j].f != '\0')
