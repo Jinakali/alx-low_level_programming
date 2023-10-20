a82482faf05b3450e53fedd3c9edbf37f9de5070
Jinakali <mutuaericmurithi@gmail.com>
2023-10-20 15:21:57 -0700

    assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,18 @@
-section .data
-	hello db "Hello, Holberton",10
+global main
 
-section .text
-	global main
+section .txt
 
 main:
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, hello
-	mov rdx, 17
-	syscall
+	mov rsi, str
+	mov rdx, str_len
+	SYSCALL; call write
 
-	mov rax, 60
-	xor rdi, rdi
-	syscall
+	mov rax, 0x3c;call exit
+	mov rdi, 0; exit with 0
+	SYSCALL;exit(0)
+	
+section .data
+	str: DB "Hello, Holberton", 10
+	str_len: EQU $ - str;equate str_len with len of string
